7410e67235e5239759eec9498c3e261068f76bf3
katorie <katoriexxxkatorie@gmail.com>
2014-08-13 19:35:29 +0900

    modify db/seed.rb to create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# add seed data of members
+
+nickname = %w(yucao kitamuuu highwide shiraichi_ryota bonbon0605 shokola pupupopo talktome katorie)
+0.upto(8) do |m|
+  Member.create(nickname: nickname[m],
+                provider: "github",
+                uid: m.to_s + "_" + nickname,
+                image: "https://pbs.twimg.com/profile_images/3233823646/8e7ca364ecae414eb509b12915458e8f_400x400.jpeg"
+               )
+end
