12a233fb0f3b2a323c4a8d00414d05ddea06f4e8
Takahiro Uchiyama <hochweit728@gmail.com>
2014-08-16 08:36:15 +0900

    fix typo

--- db/migrate/20140815174340_create_votes.rb
+++ db/migrate/20140815174340_create_votes.rb
@@ -3,7 +3,7 @@ class CreateVotes < ActiveRecord::Migration
     create_table :votes do |t|
       t.text :comment
       t.integer :voted_member_id, :null => false
-      t.integer :voting_member_id :null =>false
+      t.integer :voting_member_id, :null =>false
 
       t.timestamps
     end
