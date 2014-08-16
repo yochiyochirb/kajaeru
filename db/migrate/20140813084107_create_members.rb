8039cb8425804142386451105c5e0964d6469bc3
Takahiro Uchiyama <hochweit728@gmail.com>
2014-08-17 01:38:32 +0900

    Modify old hash syntax

--- db/migrate/20140813084107_create_members.rb
+++ db/migrate/20140813084107_create_members.rb
@@ -1,10 +1,10 @@
 class CreateMembers < ActiveRecord::Migration
   def change
     create_table :members do |t|
-      t.string :nickname, :null => false
-      t.string :provider, :null => false
-      t.string :uid, :null => false, :unique => true
-      t.string :image, :null => false
+      t.string :nickname, null: false
+      t.string :provider, null: false
+      t.string :uid, null: false
+      t.string :image, null: false
 
       t.timestamps
     end
--- db/migrate/20140815174340_create_votes.rb
+++ db/migrate/20140815174340_create_votes.rb
@@ -2,8 +2,8 @@ class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
       t.text :comment
-      t.integer :voted_member_id, :null => false
-      t.integer :voting_member_id, :null =>false
+      t.integer :voted_member_id, null: false
+      t.integer :voting_member_id, null: false
 
       t.timestamps
     end
