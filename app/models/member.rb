20f0c69af5985c700dc47485eabedff28b55db2c
Takahiro Uchiyama <hochweit728@gmail.com>
2014-08-17 01:53:51 +0900

    add uniqueness to member-uid

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,6 @@
 class Member < ActiveRecord::Base
   validates :nickname, presence: true
   validates :provider, presence: true
-  validates :uid, presence: true
+  validates :uid, presence: true, uniqueness: true
   validates :image, presence: true
 end
