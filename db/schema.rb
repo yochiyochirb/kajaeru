33c2daeb49a65a6695e974d1bf21c7b75896d762
Takahiro Uchiyama <hochweit728@gmail.com>
2014-08-17 01:46:50 +0900

    Update schema.rb by db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140815174340) do
+ActiveRecord::Schema.define(version: 20140816164338) do
 
   create_table "members", force: true do |t|
     t.string   "nickname",   null: false
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20140815174340) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "members", ["uid"], name: "index_members_on_uid", unique: true
 
   create_table "votes", force: true do |t|
     t.text     "comment"
