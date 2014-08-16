6cb8f7364171fedc2e64aecc33cc6bab570f11f2
shokola.te.mocha@gmail.com <shokola.te.mocha@gmail.com>
2014-08-16 12:22:45 +0900

    Set rooting

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Kajaeru::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'member#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
