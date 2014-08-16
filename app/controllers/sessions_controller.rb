dd4c46c6b3a7e4f7dac56b5fe01dd66071cfb745
ta1kt0me <p.wadachi@gmail.com>
2014-08-17 00:09:21 +0900

    create session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def callback
+		auth = request.env['omniauth.auth']
+		member = Member.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Member.create_with_omniauth(auth)
+		session[:user_id] = member.id
+		redirect_to root_path
+	end
+
+	def destroy
+		reset_session
+		redirect_to root_path
+	end
+end
