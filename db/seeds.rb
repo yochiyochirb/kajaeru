0f933e639b8e61a505b025382a753ae6d27fdc39
katorie <katoriexxxkatorie@gmail.com>
2014-08-17 11:29:14 +0900

    change to each method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # add seed data of members
 
 nicknames = %w(yucao kitamuuu highwide shiraichi_ryota bonbon0605 shokola pupupopo talktome katorie)
-0.upto(8) do |m|
-  Member.create(nickname: nickname[m],
+nicknames.each do |nickname|
+  Member.create(nickname: nickname, 
                 provider: "github",
                 uid: m.to_s + "_" + nickname,
                 image: "https://pbs.twimg.com/profile_images/3233823646/8e7ca364ecae414eb509b12915458e8f_400x400.jpeg"
