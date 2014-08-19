592ea64bf758a3331fba13476f90150be7dc52f0
ta1kt0me <p.wadachi@gmail.com>
2014-08-20 07:37:42 +0900

    add session controller route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Kajaeru::Application.routes.draw do
   resources :members, only: %i(index)
   root 'members#index'
   resources :votes, except: %i(index destroy)
+
+  get '/auth/:provider/callback' => 'session#callback'
+  get '/signout'                 => 'session#destroy'
 end
