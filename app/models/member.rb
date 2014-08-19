ae9935b756d47fe6a85fa0f03897a83379043b62
ta1kt0me <p.wadachi@gmail.com>
2014-08-20 07:37:00 +0900

    insert in members when login by omniauth-github

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,13 @@ class Member < ActiveRecord::Base
   validates :provider, presence: true
   validates :uid,      presence: true, uniqueness: true
   validates :image,    presence: true, uniqueness: true
+
+  def self.create_with_omniauth(auth)
+  	create! do |member|
+ 			member.provider = auth['provider']
+ 			member.uid      = auth['uid']
+ 			member.nickname = auth['info']['nickname']
+ 			member.image    = auth['info']['image']
+ 		end
+  end
 end
