4f57b01b87f11f2c9600fb2256886f3892d0340e
Takahiro Uchiyama <hochweit728@gmail.com>
2014-08-20 00:42:42 +0900

    Make visibillity better

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,4 @@
 class Vote < ActiveRecord::Base
-  validates :voted_member_id, presence: true
+  validates :voted_member_id,  presence: true
   validates :voting_member_id, presence: true, uniqueness: true
 end
