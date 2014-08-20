2d4ef513fa09fc9604cbf8074ba7c1b8778ee44c
shokola.te.mocha@gmail.com <shokola.te.mocha@gmail.com>
2014-08-20 10:14:27 +0900

    Edit routes.rb for Ruby 2.0.x

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Kajaeru::Application.routes.draw do
 
   get "members/index"
 
-  root 'members#index'
+  root "members#index"
 
-  resources :votes, except: %w(index destroy)
+  resources :votes, except: %i(index destroy)
 end
