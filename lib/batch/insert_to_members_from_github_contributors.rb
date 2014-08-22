8bcfa0b39ccf255517375372c3b6eae7b87fda8e
ta1kt0me <p.wadachi@gmail.com>
2014-08-22 03:55:49 +0000

    insert Member if not exist in members table

--- lib/batch/insert_to_members_from_github_contributors.rb
+++ lib/batch/insert_to_members_from_github_contributors.rb
@@ -1,2 +1,21 @@
 # -*- coding: utf-8 -*-
 
+require 'open-uri'
+
+res = open('https://api.github.com/repos/yochiyochirb/meetups/stats/contributors')
+
+status, msg = res.status 
+
+if status == '200'
+  contributors = ActiveSupport::JSON.decode res.read
+  contributors.each { |contributor|
+    author = contributor['author']
+    unless Member.find_by_uid(author['id'])
+      member = Member.create!(nickname: author['login'], provider: 'github', uid: author['id'], image: author['avatar_url'])
+    else
+      puts "#{author['login']} exists already."
+    end
+  } 
+else
+  puts "STATUS CODE: #{status}, MESSAGE: #{msg}"
+end
