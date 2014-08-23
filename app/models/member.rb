a988e33aeb6ec2f0c4ec979fa87bf783344e3eb3
Suguru Odai <ourqwers@gmail.com>
2014-08-23 17:45:02 +0900

    Correct a syntax

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,6 +4,6 @@ class Member < ActiveRecord::Base
   validates :uid,      presence: true, uniqueness: true
   validates :image,    presence: true, uniqueness: true
 
-  has_many :voteds, foreign_key: 'voted_member_id', class_name: Vote
+  has_many :voteds,  foreign_key: 'voted_member_id',  class_name: Vote
   has_many :votings, foreign_key: 'voting_member_id', class_name: Vote
 end
