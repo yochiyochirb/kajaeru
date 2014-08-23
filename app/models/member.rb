1b9503b2762815e5d23f1099da69e7e3a1382898
Suguru Odai <ourqwers@gmail.com>
2014-08-23 17:33:02 +0900

    Specific relationships with member and vote

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,7 @@ class Member < ActiveRecord::Base
   validates :provider, presence: true
   validates :uid,      presence: true, uniqueness: true
   validates :image,    presence: true, uniqueness: true
+
+  has_many :voteds, foreign_key: 'voted_member_id', class_name: Vote
+  has_many :votings, foreign_key: 'voting_member_id', class_name: Vote
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ActiveRecord::Base
   validates :voted_member_id,  presence: true
   validates :voting_member_id, presence: true, uniqueness: true
+
+  belongs_to :voted_member,  foreign_key: 'voted_member_id',  class_name: Member
+  belongs_to :voting_member, foreign_key: 'voting_member_id', class_name: Member
 end
