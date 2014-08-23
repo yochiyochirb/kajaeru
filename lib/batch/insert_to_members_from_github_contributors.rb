ab5e4b14b1db294466c16763212351f2102d423a
ta1kt0me <p.wadachi@gmail.com>
2014-08-23 13:31:24 +0900

    change syntax "{...}"=>"do...end"

--- lib/batch/insert_to_members_from_github_contributors.rb
+++ lib/batch/insert_to_members_from_github_contributors.rb
@@ -7,14 +7,14 @@ status, msg = res.status
 
 if status == '200'
   contributors = ActiveSupport::JSON.decode res.read
-  contributors.each { |contributor|
+  contributors.each do |contributor|
     author = contributor['author']
     unless Member.find_by_uid(author['id'])
       member = Member.create!(nickname: author['login'], provider: 'github', uid: author['id'], image: author['avatar_url'])
     else
       puts "#{author['login']} exists already."
     end
-  } 
+  end
 else
   puts "STATUS CODE: #{status}, MESSAGE: #{msg}"
 end
