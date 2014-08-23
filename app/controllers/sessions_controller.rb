a948b4479211af0f7d843ab6631de0255a52ed06
ta1kt0me <p.wadachi@gmail.com>
2014-08-23 16:22:30 +0900

    user can login only in members talbe.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,14 @@ class SessionsController < ApplicationController
 
   def callback
     auth = request.env['omniauth.auth']
-    member = Member.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Member.create_with_omniauth(auth)
-    session[:user_id] = member.id
-    redirect_to root_path
+    member = Member.find_by_provider_and_uid(auth["provider"], auth["uid"])
+    if member
+      session[:user_id] = member.id if member
+      redirect_to root_path
+    else
+      redirect_to root_path, notice: 'You have no authorization.'
+    end
+    
   end
 
   def destroy
