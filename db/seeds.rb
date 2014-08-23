a9f3c13431a8c7877c02573b92bd96fe1524fb1f
katorie <katoriexxxkatorie@gmail.com>
2014-08-23 14:32:53 +0900

    reduce member and modify for uniqueness of img tag

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,18 @@
 # add seed data of members
 
-nicknames = %w(yucao kitamuuu highwide shiraishi_ryota bonbon0605 shokola pupupopo talktome katorie)
-nicknames.each do |nickname|
-  Member.create!(nickname: nickname, 
-                provider: "github",
-                uid: "#{nickname}_uid",
-                image: "https://pbs.twimg.com/profile_images/3233823646/8e7ca364ecae414eb509b12915458e8f_400x400.jpeg"
-                )
+Member.transaction do
+  Member.delete_all
+  members = [
+    { account: 'yucao',    image: 'https://pbs.twimg.com/profile_images/3233823646/8e7ca364ecae414eb509b12915458e8f_400x400.jpeg' },
+    { account: 'kitamuuu', image: 'https://pbs.twimg.com/profile_images/3233823646/8e7ca364ecae414eb509b12915458e8f_normal.jpeg' },
+    { account: 'highwide', image: 'https://avatars0.githubusercontent.com/u/2714316?v=2&s=400' }
+  ]
+  members.each do |member|
+    Member.create!(
+      nickname: member[:account], 
+      provider: "github",
+      uid: "#{member[:account]}_uid",
+      image: member[:image]
+    )
+  end
 end
