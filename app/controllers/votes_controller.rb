7c36c5f8cfc85be6089155e1421da50b14e92d85
Suguru Odai <ourqwers@gmail.com>
2014-08-25 00:42:21 +0900

    Rename #set_member to #set_members

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,5 @@
 class VotesController < ApplicationController
-  before_action :set_member, only: %i(new edit)
+  before_action :set_members, only: %i(new edit)
 
   def new
     @vote = Vote.new
@@ -18,7 +18,7 @@ class VotesController < ApplicationController
     if @vote.update_attributes(vote_params)
       redirect_to @vote, notice: 'Vote was successfully updated.'
     else
-      set_member
+      set_members
       render :edit
     end
   end
@@ -40,7 +40,7 @@ class VotesController < ApplicationController
     params.require(:vote).permit(:voted_member_id, :comment)
   end
 
-  def set_member
+  def set_members
     @members = Member.all
   end
 end
