79631ead7da78391e8ba20dfefca8b22982b7f32
ta1kt0me <p.wadachi@gmail.com>
2014-08-29 02:48:16 +0900

    add Vote#sum_up method

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,18 @@
 class Vote < ActiveRecord::Base
   validates :voted_member_id,  presence: true
   validates :voting_member_id, presence: true, uniqueness: true
+
+  def self.sum_up
+    group(:voted_member_id).count(:voted_member_id).inject(Array.new) do |voute_totals, elem|
+      # elem => [5, 10]
+      user_id,total = elem
+      member  = Member.where(id: user_id).first
+      voute_totals.push(Hash[
+                      nickname: member[:nickname],
+                      image:    member[:image],
+                      score:    total,
+                      comments: where(voted_member_id: user_id).pluck(:comment)
+                      ])
+    end
+  end
 end
