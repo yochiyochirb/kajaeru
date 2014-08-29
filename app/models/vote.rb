8d827be7573692fdf772289e0c4e57742cdcddee
ta1kt0me <p.wadachi@gmail.com>
2014-08-30 02:56:30 +0900

    update sort logic to desc from asc

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,7 +3,7 @@ class Vote < ActiveRecord::Base
   validates :voting_member_id, presence: true, uniqueness: true
 
   def self.total
-    vote_counts_and_users = group(:voted_member_id).count(:voted_member_id).sort{|a,b| a[1]<=>b[1]}
+    vote_counts_and_users = group(:voted_member_id).count(:voted_member_id).sort{|a,b| b[1]<=>a[1]}
     vote_counts_and_users.inject([]) do |vote_totals, vote_count_and_user|
       # vote_count_and_user => [5, 10]
       user_id,total = vote_count_and_user
