2b8d0a08ccbcf5c914d68aaba78086135e0b5f2f
Hirofumi Wakasugi <baenej@gmail.com>
2016-03-08 00:51:42 +0900

    Mock OmniAuth to skip actual authentication in development

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,33 +2,18 @@ class SessionsController < ApplicationController
   def new
   end
 
+  # OAuth 認証時のコールバック先アクション
+  # NOTE development 環境では実際には OAuth 認証は行われない
+  #      詳しくは config/initializers/omniauth.rb を参照
   def callback
-    if Rails.env.production?
-      auth = request.env['omniauth.auth']
+    auth = request.env['omniauth.auth']
+    member = Member.find_by_provider_and_uid(auth['provider'], auth['uid'])
 
-      # NOTE 最初にバッチで GH organization のメンバーをすべて入れている前提で、新しく作ったりする
-      #      ことは想定していないため、find_or_create みたいなことはしていない
-      # NOTE 上記の理由から、ローカル環境で GH ログインをする際にはあらかじめバッチを実行しておく
-      #      必要がある（だから wiki の開発環境構築手順にそれが書いてある）。
-      #      しかし、実際のコードでは RAILS_ENV を見て分岐して、production でなければ特にそういった
-      #      手順をとらなくてもログインができるように、session には無理やり id をつめている。
-      #      session に手で id をつめるなら、そもそも /auth/github にいかなくていいし
-      #      GH OAutn app の登録はしなくていいと思うんだけど...
-      #      案としては、auth_path みたいなルーティングを作って、そのなかで RAILS_ENV を見て
-      #      redirect_to '/auth/github/' に向かせるのと（production）、def set_member_info_for_dev_and_test
-      #      みたいなアクションで session をつめるのと、かなあ。
-      member = Member.find_by_provider_and_uid(auth['provider'], auth['uid'])
-
-      if member
-        session[:user_id] = member.id
-        redirect_to root_path
-      else
-        redirect_to root_path, notice: 'You have no authorization.'
-      end
-    else
-      # テストデータでログインするため実質認証はしていない
-      session[:user_id] = 1
+    if member
+      session[:user_id] = member.id
       redirect_to root_path
+    else
+      redirect_to root_path, notice: 'You have no authorization.'
     end
   end
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,14 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
+
+  # NOTE 開発時に GitHub の OAuth application の登録が必須だと大変なので、
+  #      development 環境では実際に認証はせず OmniAuth の mock 機能を利用する
+  if Rails.env.development?
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(
+      provider: 'github',
+      uid: '11111',
+      info: { nickname: 'alice' }
+    )
+  end
 end
