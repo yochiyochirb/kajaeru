0e7e3fb079854d2ce9a5f4272d24657b4f289bf4
Yuka Kato <satoimo.to.imo@gmail.com>
2016-03-12 08:49:39 +0900

    Add comment about using `ENV['GITHUB_CLIENT_ID']` and `ENV['GITHUB_CLIENT_SECRET']`

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  # NOTE 開発時に GitHub の OAuth application の登録が必須だと大変なので、
+  #      development 環境では実際に認証は行わない。
+  #      そのため、development 環境で動かすだけであれば、
+  #      GitHub 認証をする際に必要となる ENV['GITHUB_CLIENT_ID'] と ENV['GITHUB_CLIENT_SECRET'] には
+  #      token の値が入っていない状態でかまわない。
   provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
 
   # NOTE 開発時に GitHub の OAuth application の登録が必須だと大変なので、
