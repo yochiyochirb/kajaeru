ffda81946402c7b594d36fd6d53ff46527c673dd
Yuka Kato <satoimo.to.imo@gmail.com>
2016-03-15 01:50:35 +0900

    Configuration for running Minitest

---
+++ test/test_helper.rb
@@ -0,0 +1,7 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+# Setting for minitest-reporters
+require 'minitest/reporters'
+Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new
