f193d167a847d743cb81553460568bb255cd476a
Hirofumi Wakasugi <baenej@gmail.com>
2016-03-14 23:25:21 +0900

    Use delegators to access Member's methods on Role

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,10 @@
+require 'forwardable'
+
 class Role < ActiveRecord::Base
+  # NOTE Role (Candidate/Voter) から透過的に Member の属性に
+  #      アクセスするために delegation を利用する
+  extend Forwardable
+  def_delegators :member, :nickname, :image
+
   belongs_to :member
 end
