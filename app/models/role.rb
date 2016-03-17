f77494eb406048189a491ad9c04e5b8b13782985
Hirofumi Wakasugi <baenej@gmail.com>
2016-03-17 10:51:36 +0900

    Why the heck I use pure Ruby's delegates rather than Rails'

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,10 +1,7 @@
-require 'forwardable'
-
 class Role < ActiveRecord::Base
   # NOTE Role (Candidate/Voter) から透過的に Member の属性に
   #      アクセスするために delegation を利用する
-  extend Forwardable
-  def_delegators :member, :nickname, :image
+  delegate :nickname, :image, to: :member
 
   belongs_to :member
 end
