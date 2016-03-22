f5d8a04181e38bb0ef1a8502781ad0041d936e19
Hirofumi Wakasugi <baenej@gmail.com>
2016-03-22 22:47:35 +0900

    Apply fixtures changes to tests

--- test/votes_controller_test.rb
+++ test/votes_controller_test.rb
@@ -4,10 +4,10 @@ class VotesControllerTest < ActionDispatch::IntegrationTest
   test 'vote by current_user' do
     get '/votes/new'
 
-    ApplicationController.stub_any_instance(:current_user, roles(:alice_voter)) do
-      post votes_url, params: { vote: { candidate_id: roles(:bob_candidate).id, comment: 'thank you' } }
+    ApplicationController.stub_any_instance(:current_user, voters(:alice_voter)) do
+      post votes_url, params: { vote: { candidate_id: candidates(:bob_candidate).id, comment: 'thank you' } }
       # XXX Vote.first だと期待しないデータがとれてしまうことが今後あるかもしれない
-      assert_equal roles(:alice_voter).id, Vote.first.voter_id
+      assert_equal voters(:alice_voter).id, Vote.first.voter_id
     end
   end
 end
