0d39d8111dbddae9d2aa7c70ad5b1536bff38ae0
ta1kt0me <p.wadachi@gmail.com>
2016-03-28 23:20:47 +0900

    Fix TODO to weak to change

--- test/votes_controller_test.rb
+++ test/votes_controller_test.rb
@@ -2,11 +2,9 @@ require 'test_helper'
 
 class VotesControllerTest < ActionDispatch::IntegrationTest
   test 'vote by current_user' do
-
     ApplicationController.stub_any_instance(:current_user, voters(:alice_voter)) do
       post votes_url, params: { vote: { candidate_id: candidates(:bob_candidate).id, comment: 'thank you' } }
-      # XXX Vote.first だと期待しないデータがとれてしまうことが今後あるかもしれない
-      assert_equal voters(:alice_voter).id, Vote.first.voter_id
+      assert_equal members(:bob), Vote.find_by(voter: voters(:alice_voter)).candidate.member
     end
   end
 end
