e0048b5e223f1524951d60971ce0be6d61e30a1f
ta1kt0me <p.wadachi@gmail.com>
2016-03-31 00:51:08 +0900

    Fix expectation to follow a spec

--- test/votes_controller_test.rb
+++ test/votes_controller_test.rb
@@ -4,7 +4,7 @@ class VotesControllerTest < ActionDispatch::IntegrationTest
   test 'vote by current_user' do
     ApplicationController.stub_any_instance(:current_user, voters(:alice_voter)) do
       post votes_url, params: { vote: { candidate_id: candidates(:bob_candidate).id, comment: 'thank you' } }
-      assert_equal members(:bob), Vote.find_by(voter: voters(:alice_voter)).candidate.member
+      assert_equal voters(:alice_voter), Vote.last.voter
     end
   end
 end
