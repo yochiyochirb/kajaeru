6ab3eac4ed6b6c7727022ec74081555cfc59eeaa
Hirofumi Wakasugi <baenej@gmail.com>
2016-03-30 13:23:40 +0900

    Change member's association from roles to candidates and voters

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,10 +1,5 @@
 class Member < ActiveRecord::Base
-  # validates :nickname, presence: true, uniqueness: true
-  # validates :provider, presence: true
-  # validates :uid,      presence: true, uniqueness: true
-  # validates :image,    presence: true, uniqueness: true
+  has_many :candidates
+  has_many :voters
 
-  has_many :roles
-  # XXX なんだろう以下のコード、あってるんだろうか？
-  # has_many :candidate, -> { roles.where(_type: 'Candidate') }
 end
