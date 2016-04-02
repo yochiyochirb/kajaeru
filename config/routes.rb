00cf8311aa8c7c70b40ce3b5a60726c437971d44
Hirofumi Wakasugi <baenej@gmail.com>
2016-04-02 12:08:02 +0900

    Move votes routes inside events

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :members, only: %i(index)
   root 'members#index'
-  get 'votes/total'
-  resources :votes, except: %i(index destroy)
 
   resources :events, only: %i(index show) do
     resources :candidates, shallow: true, only: %i(index)
     resources :voters, shallow: true, only: %i(index)
+    resources :votes, shallow: true, except: %i(index destroy)
+    get 'votes/total'
   end
 
   get '/auth/:provider/callback' => 'sessions#callback'
