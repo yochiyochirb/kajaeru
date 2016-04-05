4beba777ae20f24b5c531d89418f618152beff07
Yuka Kato <satoimo.to.imo@gmail.com>
2016-04-05 13:33:14 +0900

    Insert spaces

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
 module ApplicationHelper
   def bootstrap_alert_class(flash_type)
-    {alert: 'alert-danger', notice: 'alert-info'}.fetch(flash_type.to_sym, '')
+    {
+      alert: 'alert-danger',
+      notice: 'alert-info'
+    }.fetch(flash_type.to_sym, '')
   end
 end
