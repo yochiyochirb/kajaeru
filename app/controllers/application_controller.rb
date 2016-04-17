6b527fa8fc77258b41316df641ac0f2cdbaf1842
Yuka Kato <satoimo.to.imo@gmail.com>
2016-04-17 23:54:18 +0900

    Remove unused parameter `exception`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,8 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def render_404(exeption = nil)
-    if exeption
-      render file: File.join(Rails.root, 'public', '404.html'), status: 404
-    end
+  def render_404
+    render file: File.join(Rails.root, 'public', '404.html'), status: 404
   end
 
   def current_user
