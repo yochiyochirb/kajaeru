9b89acdd8deb04d611f20f9d921e05c6e8b30da7
Yuka Kato <satoimo.to.imo@gmail.com>
2016-04-23 19:10:11 +0900

    Add total_opened column to events table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329153713) do
+ActiveRecord::Schema.define(version: 20160423100734) do
 
   create_table "events", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",                         null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.boolean  "total_opened", default: false, null: false
   end
 
   create_table "members", force: :cascade do |t|
